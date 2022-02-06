56b54ae2dba97fc098be010c36302383d461bea9
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-06 16:26:41 +0100

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="text-center bg-purple-400">
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
