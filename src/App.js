e67ab1bc9b2927e3c7c25c613af030185a5b9376
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-06 20:21:28 +0100

    Routes and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from "react";
+//   React Router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//   Importing Layouts
+import Footer from "./components/layouts/Footer";
+import Navbar from "./components/layouts/Navbar";
+//   Importing Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="text-center bg-purple-400">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
