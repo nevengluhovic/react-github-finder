65415ba159d05f7fe8a84cacb8d158a9732bfd49
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-07 19:47:23 +0100

    GithubContext

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,21 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Spinner from "../layouts/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
+  // Fetching users on first load
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`https://api.github.com/users`);
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
