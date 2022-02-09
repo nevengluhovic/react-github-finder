8d8e32028eede5f1a7f9731d16a809f7aac5be7e
Neven Gluhovic <nevengluhovic@gmail.com>
2022-02-09 14:12:59 +0100

    removing env

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 // Get search users
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  const response = await axios.get(`${GITHUB_URL}/search/users?${params}`);
+  const response = await axios.get(
+    `https://api.github.com/search/users?${params}`
+  );
   return response.data.items;
 };
 
@@ -20,9 +19,9 @@ export const getUserAndRepos = async (login) => {
   });
 
   const [user, repos] = await Promise.all([
-    await axios.get(`${GITHUB_URL}/users/${login}`),
+    await axios.get(`https://api.github.com/users/${login}`),
     await axios.get(
-      `${GITHUB_URL}/users/${login}/repos?sort=created&per_page=10`
+      `https://api.github.com/users/${login}/repos?sort=created&per_page=10`
     ),
   ]);
   return { user: user.data, repos: repos.data };
